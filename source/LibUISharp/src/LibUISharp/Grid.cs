54150976c051e971be97c80486586b6d6923d1c9
Thomas Corwin <tacorwin@outlook.com>
2018-03-30 17:33:28 -0400

    Finished Grid.

---
+++ source/LibUISharp/src/LibUISharp/Grid.cs
@@ -0,0 +1,29 @@
+﻿using LibUISharp.Collections;
+using static LibUISharp.Internal.LibUI;
+
+namespace LibUISharp
+{
+    public class Grid : ContainerControl<GridItemCollection, Grid>
+    {
+        private bool padded;
+
+        public Grid() => Handle = uiNewGrid();
+
+        public bool Padded
+        {
+            get
+            {
+                padded = uiGridPadded(Handle);
+                return padded;
+            }
+            set
+            {
+                if (padded != value)
+                {
+                    uiGridSetPadded(Handle, value);
+                    padded = value;
+                }
+            }
+        }
+    }
+}
