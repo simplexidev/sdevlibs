245ddffef8b9e6a1c0273b674308e79aa1f2a4a7
Thomas Corwin <tacorwin@outlook.com>
2018-05-23 22:59:21 -0400

    Basic files for this. Just a starting point, I still don't know mstest

---
+++ tests/LibUISharp.UnitTests/LibUISharp_DummyTestFile.cs
@@ -0,0 +1,10 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LibUISharp.UnitTests
+{
+    [TestClass]
+    public class LibUISharp_DummyTestFile
+    {
+        public LibUISharp_DummyTestFile() { }
+    }
+}
