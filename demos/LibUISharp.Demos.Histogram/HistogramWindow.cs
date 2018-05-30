1009bf4b23e580bb3e2227d993e7c51ab6bb25c8
Thomas Corwin <tacorwin@outlook.com>
2018-05-30 18:02:14 -0400

    Forgot a few changes in the demos.

--- demos/LibUISharp.Demos.Histogram/HistogramWindow.cs
+++ demos/LibUISharp.Demos.Histogram/HistogramWindow.cs
@@ -19,22 +19,22 @@ namespace LibUISharp.Demos.Histogram
             Margins = true;
             Child = hPanel;
             
-            hPanel.Children.Add(vPanel);
+            hPanel.Items.Add(vPanel);
             
             for (int i = 0; i < 10; i++)
             {
                 SpinBox spinBox = new SpinBox(0, 100) { Value = new Random(DateTime.Now.Millisecond).Next(0, 101) };
                 spinBox.ValueChanged += SpinBoxOnValueChanged;
-                vPanel.Children.Add(spinBox);
+                vPanel.Items.Add(spinBox);
                 spinBoxList.Add(spinBox);
             }
             
             colorPicker.ColorChanged += (sender, args) => { histogramSurface.QueueRedrawAll(); };
 
-            vPanel.Children.Add(colorPicker);
+            vPanel.Items.Add(colorPicker);
 
             histogramSurface = new Surface(new SurfaceHandler(colorPicker, spinBoxList));
-            hPanel.Children.Add(histogramSurface, true);
+            hPanel.Items.Add(histogramSurface, true);
         }
 
         private void SpinBoxOnValueChanged(object sender, EventArgs eventArgs) => histogramSurface.QueueRedrawAll();
