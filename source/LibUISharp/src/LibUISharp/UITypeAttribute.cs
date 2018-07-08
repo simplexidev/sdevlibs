5edbd3c191c3cf95ee3d25422df22d2ae5af3439
Thomas Corwin <tacorwin@outlook.com>
2018-07-07 23:42:06 -0400

    Added a UIType attribute that is purely used for informational use.

---
+++ source/LibUISharp/src/LibUISharp/UITypeAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LibUISharp
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Delegate | AttributeTargets.Enum | AttributeTargets.Interface, Inherited = false, AllowMultiple = false)]
+    internal sealed class UITypeAttribute : Attribute
+    {
+        public UITypeAttribute(string typeName) => TypeName = typeName;
+
+        public string TypeName { get; }
+    }
+}
