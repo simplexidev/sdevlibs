f7fc79855c0ae098464b084d2dc36847f28f993d
Thomas Corwin <tacorwin@outlook.com>
2018-08-21 12:55:28 -0400

    Added UIComponentInvalidHandleException, that may or may not work.

--- source/LibUISharp/src/LibUISharp/UIException.cs
+++ source/LibUISharp/src/LibUISharp/UIException.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Runtime.InteropServices;
 using System.Runtime.Serialization;
+using LibUISharp.Internal;
+using LibUISharp.SafeHandles;
 
 namespace LibUISharp
 {
     /// <summary>
-    /// Represents a UI-related exception.
+    /// The basic exception type for all UI-related exception.
     /// </summary>
     [Serializable]
     public class UIException : ExternalException
@@ -35,5 +37,58 @@ namespace LibUISharp
         /// <param name="serializationInfo">The <see cref="SerializationInfo"/> that holds the serialized object data about the exception being thrown.</param>
         /// <param name="streamingContext">The <see cref="StreamingContext"/> that contains contextual information about the source or destination.</param>
         protected UIException(SerializationInfo serializationInfo, StreamingContext streamingContext) : base(serializationInfo, streamingContext) { }
+    }
+
+    /// <summary>
+    /// The exception that is thrown when a <see cref="UIComponent{T}"/> object's handle is null or invalid.
+    /// </summary>
+    [Serializable]
+    public class UIHandleInvalidException<T> : UIException
+        where T : SafeHandleZeroIsInvalid
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UIHandleInvalidException"/> class.
+        /// </summary>
+        public UIHandleInvalidException() { }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UIHandleInvalidException"/> class with the specified error message.
+        /// </summary>
+        /// <param name="component">The component whose handle is invalid.</param>
+        public UIHandleInvalidException(UIComponent<T> component) : base() => Component = component;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UIHandleInvalidException"/> class with the specified error message.
+        /// </summary>
+        /// <param name="component">The component whose handle is invalid.</param>
+        /// <param name="message">The error message that specifies the reason for the exception.</param>
+        public UIHandleInvalidException(UIComponent<T> component, string message) : base(message) => Component = component;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UIHandleInvalidException"/> class with the specified error message
+        /// and <see langword="abstract"/>reference to the inner exception that is the cause of this exception.
+        /// </summary>
+        /// <param name="component">The component whose handle is invalid.</param>
+        /// <param name="message">The error message that specifies the reason for the exception.</param>
+        /// <param name="inner">The exception that is the cause of the current exception.</param>
+        public UIHandleInvalidException(UIComponent<T> component, string message, Exception inner) : base(message, inner) => Component = component;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UIHandleInvalidException"/> class with serialized data.
+        /// </summary>
+        /// <param name="serializationInfo">The <see cref="SerializationInfo"/> that holds the serialized object data about the exception being thrown.</param>
+        /// <param name="streamingContext">The <see cref="StreamingContext"/> that contains contextual information about the source or destination.</param>
+        protected UIHandleInvalidException(SerializationInfo serializationInfo, StreamingContext streamingContext) : base(serializationInfo, streamingContext) { }
+
+        public UIComponent<T> Component { get; }
+
+        public string NativeType
+        {
+            get
+            {
+                NativeTypeAttribute attr = (NativeTypeAttribute)Attribute.GetCustomAttribute(Component.GetType(), typeof(NativeTypeAttribute));
+                return attr.Name;
+            }
+        }
     }
 }
