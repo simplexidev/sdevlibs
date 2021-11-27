c1f63244da8044a3c84641aca81a39262dd5a954
tom-corwin <tacorwin@outlook.com>
2021-11-26 23:06:53 -0500

    Added a `NativeComponent` type.

---
+++ sources/LibUISharp.Core/src/LibUISharp/Runtime/InteropServices/INativeComponent.cs
@@ -0,0 +1,17 @@
+﻿/***********************************************************************************************************************
+ * FileName:            INativeComponent.cs
+ * Copyright/License:   https://github.com/tom-corwin/libuisharp/blob/master/LICENSE.md
+***********************************************************************************************************************/
+
+using LibUISharp.ComponentModel;
+
+namespace LibUISharp.Runtime.InteropServices
+{
+    public interface INativeComponent : IComponent
+    {
+        unsafe void* Handle { get; }
+
+        void InitializeHandle();
+        void DisposeHandle();
+    }
+}
---
+++ sources/LibUISharp.Core/src/LibUISharp/Runtime/InteropServices/INativeComponent`1.cs
@@ -0,0 +1,17 @@
+﻿/***********************************************************************************************************************
+ * FileName:            INativeComponent`1.cs
+ * Copyright/License:   https://github.com/tom-corwin/libuisharp/blob/master/LICENSE.md
+***********************************************************************************************************************/
+
+using LibUISharp.ComponentModel;
+
+namespace LibUISharp.Runtime.InteropServices
+{
+    public interface INativeComponent<T> : IComponent where T : unmanaged
+    {
+        unsafe T* Handle { get; }
+
+        void InitializeHandle();
+        void DisposeHandle();
+    }
+}
